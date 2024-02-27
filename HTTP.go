c53eeb5137e9c03a69bec5f1183c23c9b62fc7c8
Anton <ponochovnyi.anton@LLL.kpi.ua>
2024-02-27 19:10:11 +0200

    init commit

---
+++ HTTP.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+	"time"
+)
+
+type TimeResponse struct {
+	Time string `json:"time"`
+}
+
+func timeHandler(w http.ResponseWriter, r *http.Request) {
+	currentTime := time.Now().Format(time.RFC3339)
+	response := TimeResponse{Time: currentTime}
+	jsonResponse, err := json.Marshal(response)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(jsonResponse)
+}
+
+func main() {
+	http.HandleFunc("/time", timeHandler)
+	http.ListenAndServe(":8795", nil)
+}
